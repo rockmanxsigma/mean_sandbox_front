ff1c2b586de9d086eff21beed012807d0028ced7
Jeremie Roubeau <rockmanxsigma@gmail.com>
2025-06-19 17:17:29 +0200

    Add PrimeUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import PrimeUI from "tailwindcss-primeui"
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
@@ -5,5 +7,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [PrimeUI],
 }
