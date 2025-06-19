05ea331ab49eed2cbce63152c5e6c18309a7f3a0
Jeremie Roubeau <rockmanxsigma@gmail.com>
2025-06-19 17:19:24 +0200

    Add PrimeUI imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    }),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes)
   ]
-};
+}
