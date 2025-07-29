90d03dd4fb4cf483c5ede020dfb67d9e60abb1ba
Jeremie Roubeau <rockmanxsigma@gmail.com>
2025-07-29 16:19:43 +0200

    Add Navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 import { ButtonDemo } from './components/button-demo/button-demo'
 import { Things } from './components/things/things'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonDemo, Things],
+  imports: [RouterOutlet, ButtonDemo, Navbar, Things],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
